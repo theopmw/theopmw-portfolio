48d7f86e3de5db471e8d7990d5d5cd4cf4331c78
theopmw <theowrightdev@gmail.com>
2022-10-31 17:34:38 +0000

    Add home page content

---
+++ static/js/home.js
@@ -0,0 +1,16 @@
+document.getElementById('hero').addEventListener('mousemove', (event) => {
+  // document.body.addEventListener("mousemove", event => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  gsap.set('.cursor', {
+    x: mouseX,
+    y: mouseY,
+  });
+
+  gsap.to('.shape', {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1,
+  });
+});
