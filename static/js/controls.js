69a2bd6bea587c04a9fbbfdf6b31b747aba2c5fb
theopmw <theowrightdev@gmail.com>
2022-10-31 16:25:30 +0000

    Add logic to add/remove active class on each section

--- static/js/controls.js
+++ static/js/controls.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 const pageTransitions = () => {
   // Active button on click
@@ -12,9 +12,31 @@ const pageTransitions = () => {
         'active-btn',
         ''
       );
-      this.className += ' active-btn';
+      this.classList += ' active-btn';
     });
   }
+
+  // Toggle section active class
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // Remove active class from other buttons
+      sectBtn.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+
+      // Add active class to button on click event
+      e.target.classList.add('active');
+
+      // Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+      // Add active class
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 };
 
 // function pageTransitions() {
