53a8583b19bb13feb10464edc5537a26bd903260
theopmw <theowrightdev@gmail.com>
2022-11-16 12:18:51 +0000

    Add Databases section to skills

--- static/js/skills.js
+++ static/js/skills.js
@@ -10,24 +10,32 @@ const frameworks = [
   'Django',
 ];
 
+const databases = ['MongoDB', 'Postgres', 'MySQL'];
+
 const versionControl = ['Git', 'GitHub'];
 
 const design = ['Balsamiq', 'Figma'];
+
+const other = ['Heroku', 'Stripe', 'AWS'];
 
 // set variables
 let imgType = '.svg';
 let imgs_languages = './assets/img/languages/';
 let imgs_frameworks = './assets/img/frameworks/';
+let imgs_databases = './assets/img/databases/';
 let imgs_version_control = './assets/img/version-control/';
 let imgs_design = './assets/img/design/';
 let ul_languages = "<div class='languages-container'><ul>";
 let ul_frameworks = "<div class='frameworks-container'><ul>";
+let ul_databases = "<div class='databases-container'><ul>";
 let ul_version_control = "<div class='version-control-container'><ul>";
 let ul_design = "<div class='design-container'><ul>";
 
 let languagesList = ul_languages;
 
 let frameworksList = ul_frameworks;
+
+let databasesList = ul_databases;
 
 let versionControlList = ul_version_control;
 
@@ -42,6 +50,11 @@ frameworks.map((framework) => {
   frameworksList += `<li class="skills-item"><img src="${imgs_frameworks}${framework}${imgType}" alt="${framework}"><h3>${framework}</h3></li>`;
 });
 ul_frameworks += '</ul></div>';
+
+databases.map((database) => {
+  databasesList += `<li class="skills-item"><img src="${imgs_databases}${database}${imgType}" alt="${database}"><h3>${database}</h3></li>`;
+});
+ul_databases += '</ul></div>';
 
 versionControl.map((version) => {
   versionControlList += `<li class="skills-item"><img src="${imgs_version_control}${version}${imgType}" alt="${version}"><h3>${version}</h3></li>`;
@@ -61,10 +74,14 @@ languagesDiv.innerHTML += languagesList;
 let frameworksDiv = document.getElementById('frameworks');
 frameworksDiv.innerHTML += frameworksList;
 
+// append all databases to #databases
+let databasesDiv = document.getElementById('databases');
+databasesDiv.innerHTML += databasesList;
+
 // append all version control to #version-control
 let versionControlDiv = document.getElementById('version-control');
 versionControlDiv.innerHTML += versionControlList;
 
-// append all version control to #version-control
+// append all version control to #design
 let designDiv = document.getElementById('design');
 designDiv.innerHTML += designList;
